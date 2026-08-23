6a89d7f5c23d35be5eb208ba948f9e517f55780f
Movses5555 <mosisrayelyan@gmail.com>
2026-08-23 23:45:26 +0400

    changed hero section image

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -32,7 +32,7 @@ export default function Hero() {
         <Reveal immediate delay={450} y={32} className="mt-8 md:mt-12 lg:mt-14 w-full max-w-[1248px]">
           <div className="relative aspect-[1248/570] w-full overflow-hidden bg-[#F9FAFB]">
             <Image
-              src="/hero_preview.webp"
+              src="/hero_preview.svg"
               alt="MA UI Kit Dashboard Preview"
               fill
               priority
